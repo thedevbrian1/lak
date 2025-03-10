53087d4f7e4b94be4c1ca44f1f359998ae415c21
thedevbrian1 <brayomwas95@gmail.com>
2025-03-10 22:48:29 +0300

    Basic validation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
-import { Form, Link, NavLink } from "react-router";
+import { Form, isRouteErrorResponse, Link, NavLink } from "react-router";
 import { services } from "~/utils/services";
-import { ArrowRight, Mail, Phone } from "lucide-react";
+import { ArrowRight, Mail, Phone, RotateCcw } from "lucide-react";
 import FormSpacer from "~/components/FormSpacer";
 import { Label } from "~/components/ui/label";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { Textarea } from "~/components/ui/textarea";
+import { ErrorIllustration } from "~/components/Icon";
+import {
+  badRequest,
+  trimString,
+  validateEmail,
+  validatePhone,
+  validateText,
+} from "~/.server/validation";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -20,14 +28,44 @@ export function loader() {
   return { message: "Hello from Vercel" };
 }
 
-export default function Home() {
+export async function action({ request }: Route.ActionArgs) {
+  let formData = await request.formData();
+  let name = String(formData.get("name"));
+  let email = String(formData.get("email"));
+  let phone = String(formData.get("phone"));
+  let message = String(formData.get("message"));
+
+  let trimmedPhone = trimString(phone);
+
+  let fieldErrors = {
+    name: validateText(name),
+    email: validateEmail(email),
+    phone: validatePhone(trimmedPhone),
+    message: validateText(message),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors });
+  }
+  return null;
+}
+
+export default function Home({ actionData }: Route.ComponentProps) {
+  let fieldErrors;
+  if (
+    actionData &&
+    typeof actionData === "object" &&
+    "fieldErrors" in actionData
+  ) {
+    fieldErrors = actionData?.fieldErrors;
+  }
   return (
     <main className="">
       <Hero />
       <BriefServices />
       <Locations />
       <Clients />
-      <HomeContact />
+      <HomeContact fieldErrors={fieldErrors} />
     </main>
   );
 }
@@ -225,7 +263,7 @@ function Clients() {
   );
 }
 
-function HomeContact() {
+function HomeContact({ fieldErrors }) {
   return (
     <div className="mt-24 lg:mt-36 px-6 py-14 md:py-16 lg:py-24 max-w-xs md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto bg-gray-200 dark:bg-[#333333] dark:text-white rounded-lg">
       <h2 className="font-semibold font-heading text-3xl md:text-4xl lg:text-5xl text-center">
@@ -274,35 +312,78 @@ function HomeContact() {
           <Form method="post" className="mt-6">
             <fieldset className="space-y-4">
               <FormSpacer>
-                <Label htmlFor="name">Full name</Label>
+                <Label htmlFor="name" className="flex gap-2">
+                  Full name{" "}
+                  {fieldErrors?.name ? (
+                    <span className="text-red-500">{fieldErrors.name}</span>
+                  ) : (
+                    ""
+                  )}
+                </Label>
                 <Input
                   type="text"
                   id="name"
                   name="name"
                   placeholder="John Doe"
+                  className={`border border-gray-400 focus-visible:ring-2 focus-visible:ring-brand-purple ${
+                    fieldErrors?.name ? "border-red-500" : ""
+                  }`}
                 />
               </FormSpacer>
               <FormSpacer>
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email" className="flex gap-2">
+                  Email{" "}
+                  {fieldErrors?.email ? (
+                    <span className="text-red-500">{fieldErrors.email}</span>
+                  ) : (
+                    ""
+                  )}
+                </Label>
                 <Input
                   type="email"
                   id="email"
                   name="email"
                   placeholder="johndoe@email.com"
+                  className={`border border-gray-400 focus-visible:ring-2 focus-visible:ring-brand-purple ${
+                    fieldErrors?.email ? "border-red-500" : ""
+                  }`}
                 />
               </FormSpacer>
               <FormSpacer>
-                <Label htmlFor="phone">Phone</Label>
+                <Label htmlFor="phone" className="flex gap-2">
+                  Phone{" "}
+                  {fieldErrors?.phone ? (
+                    <span className="text-red-500">{fieldErrors.phone}</span>
+                  ) : (
+                    ""
+                  )}
+                </Label>
                 <Input
                   type="text"
                   id="phone"
                   name="phone"
                   placeholder="+254 712 345 678"
+                  className={`border border-gray-400 focus-visible:ring-2 focus-visible:ring-brand-purple ${
+                    fieldErrors?.phone ? "border-red-500" : ""
+                  }`}
                 />
               </FormSpacer>
               <FormSpacer>
-                <Label htmlFor="message">Message</Label>
-                <Textarea id="message" name="message" />
+                <Label htmlFor="message" className="flex gap-2">
+                  Message{" "}
+                  {fieldErrors?.message ? (
+                    <span className="text-red-500">{fieldErrors.message}</span>
+                  ) : (
+                    ""
+                  )}
+                </Label>
+                <Textarea
+                  id="message"
+                  name="message"
+                  className={`border border-gray-400 focus-visible:ring-2 focus-visible:ring-brand-purple ${
+                    fieldErrors?.message ? "border-red-500" : ""
+                  }`}
+                />
               </FormSpacer>
               <Button className="w-full bg-brand-purple hover:bg-purple-500 transition ease-in-out duration-300">
                 Send message
@@ -313,4 +394,56 @@ function HomeContact() {
       </div>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    console.error(error);
+    return (
+      <div className="pt-52 lg:pt-60 px-6 flex flex-col items-center md:max-w-xl lg:max-w-2xl mx-auto">
+        <div className="w-48 lg:w-72">
+          <ErrorIllustration />
+        </div>
+        <h1 className="font-bold text-3xl lg:text-4xl text-red-500 mt-8 text-center">
+          {error.status} {error.statusText}
+        </h1>
+        <p className="text-red-500 text-center mt-4">{error.data}</p>
+        <div className="has-[:active]:scale-[0.97] transition ease-out duration-300 mt-4">
+          <Link
+            to="."
+            prefetch="intent"
+            className="bg-brand-purple hover:bg-purple-500 transition ease-in-out duration-300 px-4 py-2 rounded-lg text-white  flex gap-2 items-center"
+          >
+            <RotateCcw />
+            Try again
+          </Link>
+        </div>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    console.error(error);
+    return (
+      <div className="pt-32 lg:pt-60 px-6 flex flex-col items-center lg:max-w-2xl mx-auto">
+        <div className="w-48 lg:w-72">
+          <ErrorIllustration />
+        </div>
+        <h1 className="font-bold text-center text-3xl lg:text-4xl text-red-500 mt-8">
+          Error
+        </h1>
+        <p className="text-red-500 text-center mt-4">{error.message}</p>
+        <div className="has-[:active]:scale-[0.97] transition ease-out duration-300 mt-4">
+          <Link
+            to="."
+            prefetch="intent"
+            className="bg-brand-purple hover:bg-purple-500 transition ease-in-out duration-300 px-4 py-2 rounded-lg text-white  flex gap-2 items-center"
+          >
+            <RotateCcw />
+            Try again
+          </Link>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
